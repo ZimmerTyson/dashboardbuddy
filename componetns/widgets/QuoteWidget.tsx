14f007c9601c81231852d351cdd554168a1b5bde
Tyson Zimmer <zimmertyson@gmail.com>
2025-11-01 23:48:52 -0700

    thinking through apis and what is going to be used througout the project

--- componetns/widgets/QuoteWidget.tsx
+++ componetns/widgets/QuoteWidget.tsx
@@ -1,6 +1,12 @@
 export default function QuoteWidget() {
   // Quote widget code here
+
+  async function fetchData() {
+    const res = await fetch('');
+    const data = res.json();
+  }
   return (
+
     <div>
       
     </div>
