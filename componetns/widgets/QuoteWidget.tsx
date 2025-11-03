ccee344e97a0bd42159918861494608a1d9f8636
Tyson Zimmer <zimmertyson@gmail.com>
2025-11-02 20:20:52 -0800

    updated api call formatting to correctly handle errors witha try, catch setup and error routing

--- componetns/widgets/QuoteWidget.tsx
+++ componetns/widgets/QuoteWidget.tsx
@@ -2,8 +2,22 @@ export default function QuoteWidget() {
   // Quote widget code here
 
   async function fetchData() {
-    const res = await fetch('');
-    const data = res.json();
+    try {
+      const response = await fetch('');
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      return null;
+    }
+
+
   }
   return (
 
